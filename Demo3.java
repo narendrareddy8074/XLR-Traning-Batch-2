0ce0593176bbabb0a2f87c17de2b724ff1e44e6c
narendrareddy8074 <mnreddy659@gmail.com>
2025-01-02 12:08:17 +0530

    Add files via upload

---
+++ Demo3.java
@@ -0,0 +1,18 @@
+public class Demo3 {
+    /**
+     * Main method that determines the greatest number among three numbers.
+     */
+    public static void main(String[] main) {
+        int num1 = 20;
+        int num2 = 10;
+        int num3 = 30;
+
+        if (num1 > num2 && num1 > num3) {
+            System.out.println("num1 is the greatest number.");
+        } else if (num2 > num1 && num2 > num3) {
+            System.out.println("num2 is the greatest number.");
+        } else {
+            System.out.println("num3 is the greatest number.");
+        }
+    }
+}
