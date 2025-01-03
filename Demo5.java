22da36d434207443e0f459a37dbcb26ca232f88f
narendrareddy8074 <mnreddy659@gmail.com>
2025-01-03 09:52:10 +0530

    Add files via upload

---
+++ Demo5.java
@@ -0,0 +1,15 @@
+ public class Demo5 {
+        public static void main(String[] args) {
+            String username = "user001";
+            String pwd = "user_001";
+            
+            // Use equals() to compare strings
+            while(username.equals("user001") && pwd.equals("user_001")) {
+                System.out.println("hello user Happy new year");
+                break; // Exit the loop after printing the message
+            }
+        }
+    }
+    
+
+    
