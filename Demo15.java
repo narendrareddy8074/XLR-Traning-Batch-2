32ddeb42efcc6b001dffd6006f78a155dadb18ab
narendrareddy8074 <mnreddy659@gmail.com>
2025-01-09 20:34:29 +0530

    Nearest multiple of 100

---
+++ Demo15.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Demo15 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        int roundedNumber;
+
+        if (number >= 0) {
+            roundedNumber = ((number + 99) / 100) * 100; 
+        } else {
+            roundedNumber = ((number - 99) / 100) * 100; 
+        }
+
+        System.out.println("Rounded to the nearest hundred: " + roundedNumber);
+    }
+}
